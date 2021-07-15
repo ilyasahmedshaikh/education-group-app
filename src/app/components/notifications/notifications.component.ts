064414113f92b84d99f14b39189672c4a1d4fc16
ilyas ahmed <ilyasahmed081@gmail.com>
2021-07-15 16:49:28 +0500

    notification screen done

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsComponent implements OnInit {
 
+  List: any = [
+    { personImg: "assets/img/man.jpg", personName: "Danny Miles", personComment: "commented on your photo"},
+    { personImg: "assets/img/girl.jpg", personName: "Emma Carel", personComment: "commented on your photo"},
+    { personImg: "assets/img/man2.jpg", personName: "Jodan Kade", personComment: "commented on your photo"},
+    { personImg: "assets/img/girl2.jpg", personName: "Sofia Ella", personComment: "commented on your photo"},
+    { personImg: "assets/img/man3.jpg", personName: "Shane Miliano", personComment: "commented on your photo"},
+    { personImg: "assets/img/girl3.jpg", personName: "Mia Rosie", personComment: "commented on your photo"},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
