05e6348a10fb280bef6266396ab2dee9859b8f72
ilyas ahmed <ilyasahmed081@gmail.com>
2021-07-17 16:18:38 +0500

    activity details screen done

--- src/app/components/activities/activity-details/activity-details.component.ts
+++ src/app/components/activities/activity-details/activity-details.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityDetailsComponent implements OnInit {
 
+  List: any = [
+    { title: "Message from the UWI Inter campus guild presidents", content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."},
+    { title: "Guild voter's list check", content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
